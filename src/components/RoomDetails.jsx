6bdc9ebfe881fedc8795485c741321272da9b50b
Souravn1200 <souravn1200@gmail.com>
2023-11-07 21:08:05 +0600

    Fixed the details page bug

--- src/components/RoomDetails.jsx
+++ src/components/RoomDetails.jsx
@@ -6,8 +6,8 @@ const RoomDetails = () => {
     const {_id, room_description,price_per_night, room_size,availability,special_offers,image1, image2, image3} = useLoaderData();
 
     return (
-        <div className='flex gap-24 justify-center items-center h-screen'>
-           <div className='-ml-40'>
+        <div className='flex gap-24 justify-center h-screen'>
+           <div className=''>
 
                 <div className='flex gap-8'>
                     <img src={image1} className='h-[220px] w-[300px]' alt="" />
@@ -20,9 +20,22 @@ const RoomDetails = () => {
 
            </div>
 
-           <div>
-                <h2>{room_description}</h2>
-           </div>
+           <div className='font-serif mt-10'>
+    <h2 className='mb-3 text-2xl font-bold capitalize'>
+        {room_description}
+    </h2>
+    <h2 className='mb-3 text-xl text-gray-800 border-l-4 border-blue-500 pl-2'>
+    <span className='font-bold'> Price :</span>   {price_per_night}$
+    </h2>
+    <h2 className='mb-3 text-xl  text-gray-800 border-l-4 border-blue-500 pl-2 capitalize'>
+        <span className='font-bold'> Space :</span> {room_size}
+    </h2>
+    <h2 className='mb-3 text-xl  text-gray-800 border-l-4 border-blue-500 pl-2'>
+    <span className='font-bold'> Availability :</span> {availability} Person
+    </h2>
+   
+</div>
+
         </div>
     );
 };
